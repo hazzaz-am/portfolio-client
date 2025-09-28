6cb74bbec14d232188abbe069800792bb43e7d9e
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-09-29 01:19:16 +0600

    fix: resolve linting errors

--- components/modules/public/home/Hero.tsx
+++ components/modules/public/home/Hero.tsx
@@ -7,11 +7,11 @@ export function Hero() {
 			<BackgroundRippleEffect rows={10}/>
 			<div className="w-full px-4 absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 				<h2 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-neutral-800 md:text-4xl lg:text-7xl dark:text-neutral-100">
-					Building Tomorrow's Digital Experiences
+					Building Tomorrow&apos;s Digital Experiences
 				</h2>
 				<p className="relative z-10 mx-auto mt-4 max-w-xl text-center text-neutral-800 dark:text-neutral-500">
 					Software Developer with 1+ years creating robust applications and
-					user-centric solutions
+					user centric solutions
 				</p>
 			</div>
 		</div>
--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -10,6 +10,7 @@ import {
 import React, { useRef, useState } from "react";
 import { IconX, IconMenu2 } from "@tabler/icons-react";
 import Link from "next/link";
+import Image from "next/image";
 
 interface NavbarProps {
 	children: React.ReactNode;
@@ -195,7 +196,6 @@ export const MobileNavMenu = ({
 	children,
 	className,
 	isOpen,
-	onClose,
 }: MobileNavMenuProps) => {
 	return (
 		<AnimatePresence>
@@ -236,7 +236,7 @@ export const NavbarLogo = () => {
 			href="/"
 			className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
 		>
-			<img
+			<Image
 				src="https://assets.aceternity.com/logo-dark.png"
 				alt="logo"
 				width={30}
